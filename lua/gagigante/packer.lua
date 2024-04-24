b0d2bcb08c0dc59292b1ebc7b11f2b6e1bbc2b58
Gabriel Gigante <gabrielgigante29@gmail.com>
2024-04-24 00:22:20 -0300

    feat: setup vim-fugitive

---
+++ lua/gagigante/packer.lua
@@ -0,0 +1,70 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
+
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = { 'nvim-lua/plenary.nvim' },
+    }
+    -- use {
+       -- "nvim-neo-tree/neo-tree.nvim",
+        --branch = "v3.x",
+        --requires = { 
+          --  "nvim-lua/plenary.nvim",
+            --"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            --"MunifTanjim/nui.nvim",
+      --      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        --}
+    --}
+
+	use { "catppuccin/nvim", as = "catppuccin" }
+
+	use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+	use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+	use {
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { {"nvim-lua/plenary.nvim"} }
+	}
+
+	use 'mbbill/undotree'
+
+	use 'tpope/vim-fugitive'
+
+			
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment the two plugins below if you want to manage the language servers from neovim
+			-- {'williamboman/mason.nvim'},
+			-- {'williamboman/mason-lspconfig.nvim'},
+
+			{'neovim/nvim-lspconfig'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+
+    use 'christoomey/vim-tmux-navigator'
+end)
